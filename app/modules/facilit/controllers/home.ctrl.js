346bd3392161526959dedc07823e1676621f8106
jonathan <jonathan.s.campos@accenture.com>
2018-07-30 12:17:30 -0300

    Entrega

--- app/modules/facilit/controllers/home.ctrl.js
+++ app/modules/facilit/controllers/home.ctrl.js
@@ -3,137 +3,98 @@
 
     function HomeController(ApiService, ApiFactory){
         var vm      = this
-        vm.title = 'Home Controller'
-         vm.apiList  = [];
-         
-         vm.categoies = []
-
-        // vm.checkCategory = (data) =>{
-        //     var arr = []
-        //     if(data == 'Services'){
-        //         arr.push(data)
-        //     }return(arr)
-        // }
-
-        // vm.checkCategory = (data) =>{
-        //     vm.click = "clicked"
-        //     if(data == 'Services'){
-                
-        //         vm.apiList = vm.depServices;
-        //     }
-        // } "Services", "Marketing", "Accounting", "Business Development", "Human Resources", "Product Management",
-        //  "Engineering", "Research and Development", "Sales", "Legal", "Training", "Support"
-
-        vm.checkCategory = function(data){
-            switch(data) {
-                case "Services":
-                    vm.categoies.push(vm.depServices)
-                    break;
-                case "Marketing":
-                    vm.categoies.push(vm.depMarketing)
-                    break;
-                case "Accounting":
-                    vm.categoies.push(vm.depAccounting)
-                    break;    
-                case "Business Development":
-                    vm.categoies.push(vm.depBusinessDevelopment)
-                    break;    
-                case "Human Resources":
-                    vm.categoies.push(vm.depHumanResources)
-                    break;    
-                case "Product Management":
-                    vm.categoies.push(vm.depProductManagement)
-                    break;    
-                case "Engineering":
-                    vm.categoies.push(vm.depEngineering)
-                    break;    
-                case "Research and Development":
-                    vm.categoies.push(vm.depResearchAndDevelopment)
-                    break; 
-            }
-            
-        }
-
-        vm.filtred = function organize(data){
-            var arr2 = []
-            for (var i =0; i<data.length; i++){
-                data.forEach(function(element){
-                    arr2.push(element[i])
-                }) 
-            }return arr2     
-        }
-            // var result = []
-            // angular.forEach(vm.depMarketing, function(element){
-                
-            //     for(var i; i < element.length; i++){
-            //         result.push(element[i])
-            //     }
-            //     console.log(result)
-            // })
-            // return result
-        
-
-
-
-
+         vm.apiList  =  []         
+         vm.categories = []
+         vm.teste = []
 
         _init()
         function _init(){
-            ApiService.getApi().then(_sucessApi, _errorApi);           
+            ApiService.getApi().then(_sucessApi, _errorApi)           
         }
 
         function _sucessApi(response){
-            vm.apiList = ApiFactory.getApiToView(response);
-            // console.log(vm.apiList)
+            vm.apiList = ApiFactory.getApiToView(response)
 
-            vm.department = ApiFactory.departmentFilter(vm.apiList) 
-            // console.log(vm.department)
+            vm.department = ApiFactory.departmentFilter(vm.apiList)
 
             vm.role = ApiFactory.roleFilter(vm.apiList)
 
             vm.depServices = ApiFactory.servicesFilter(vm.apiList)
+            console.log(vm.depServices)
 
-            vm.depMarkeding = ApiFactory.marketingFilter(vm.apiList)
-            // console.log(vm.depMarkeding)
+            vm.depMarketing = ApiFactory.marketingFilter(vm.apiList)
 
             vm.depAccounting = ApiFactory.accountingFilter(vm.apiList)
-            // console.log(vm.depAccounting)
 
             vm.depBusinessDevelopment = ApiFactory.businessDevelopmentFilter(vm.apiList)
-            // console.log(vm.depBusinessDevelopment)
 
             vm.depHumanResources = ApiFactory.humanResourcesFilter(vm.apiList)
-            // console.log(vm.depHumanResources)
 
             vm.depProductManagement = ApiFactory.productManagementFilter(vm.apiList)
-            // console.log(vm.depProductManagement)
 
             vm.depEngineering = ApiFactory.engineeringFilter(vm.apiList)
-            // console.log(vm.depEngineering)
 
             vm.depResearchAndDevelopment = ApiFactory.researchAndDevelopmentFilter(vm.apiList)
-            // console.log(vm.depResearchAndDevelopment)
             
             vm.depSales = ApiFactory.salesFilter(vm.apiList)
-            // console.log(vm.depSales) 
 
             vm.depLegal = ApiFactory.legalFilter(vm.apiList)
-            // console.log(vm.depLegal)
             
             vm.depTraining = ApiFactory.trainingFilter(vm.apiList)
-            // console.log(vm.depTraining) 
             
             vm.depSupport = ApiFactory.supportFilter(vm.apiList)
-            // console.log(vm.depSupport) 
-            
-    
         }         
     
         function _errorApi(error){
             console.log(error)
         }
 
-       
+        vm.checkCategory = function(data){
+            switch(data){
+                case "Services":
+                    vm.categories.push(vm.depServices)
+                    // console.log(vm.categories)
+                    break;
+                case "Marketing":
+                    vm.categories.push(vm.depMarketing)
+                    // console.log(vm.categories)
+                    break;
+                case "Accounting":
+                    vm.categories.push(vm.depAccounting)
+                    // console.log(vm.categories)
+                    break;    
+                case "Business Development":
+                    vm.categories.push(vm.depBusinessDevelopment)
+                    // console.log(vm.categories)
+                    break;    
+                case "Human Resources":
+                    vm.categories.push(vm.depHumanResources)
+                    // console.log(vm.categories)
+                    break;    
+                case "Product Management":
+                    vm.categories.push(vm.depProductManagement)
+                    // console.log(vm.categories)
+                    break;    
+                case "Engineering":
+                    vm.categories.push(vm.depEngineering)
+                    // console.log(vm.categories)
+                    break;    
+                case "Research and Development":
+                    vm.categories.push(vm.depResearchAndDevelopment)
+                    // console.log(vm.categories)
+                    break;
+            }                       
+        }               
+
+        vm.filtred = function organize(data){
+            var arr = []
+            for (var i =0; i<data.length; i++){
+                data.forEach(function(element){
+                    vm.teste.push(element[i])
+                }) 
+            } console.log(vm.teste)
+        
+        }       
     }
     angular.module("facilit").controller("HomeController", HomeController);
 })();
